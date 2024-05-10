dd937908cb7118a6c342f2a0b663bf3c6c8157c2
ciubotarum <ciubotarum@outlook.com>
2024-05-10 11:36:26 +0300

    Reverse The String

---
+++ src/strings/ReversedStrings.java
@@ -0,0 +1,24 @@
+package strings;
+
+public class ReversedStrings {
+//    Complete the solution so that it reverses the string passed into it.
+//
+//            'world'  =>  'dlrow'
+//            'word'   =>  'drow'
+
+    public static String solution(String str) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            builder.append(str.charAt(i));
+        }
+
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "world";
+
+        System.out.println(solution(str));
+    }
+}
