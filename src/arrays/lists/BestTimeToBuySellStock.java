fa88768cb411a9c04a756822aa5a5b4b9a5380b1
ciubotarum <ciubotarum@outlook.com>
2024-10-04 17:01:30 +0300

    Solved problem to find correctly the best total profit

---
+++ src/arrays/lists/BestTimeToBuySellStock.java
@@ -0,0 +1,44 @@
+package arrays.lists;
+
+import java.util.Arrays;
+
+public class BestTimeToBuySellStock {
+//    You are given an array prices where prices[i] is the price of a given stock on the ith day.
+//
+//    You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the
+//    future to sell that stock.
+//
+//    Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+//    Example 1:
+//    Input: prices = [7,1,5,3,6,4]
+//    Output: 5
+//    Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+//    Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+//    Example 2:
+//    Input: prices = [7,6,4,3,1]
+//    Output: 0
+//    Explanation: In this case, no transactions are done and the max profit = 0.
+
+    static int findMaxProfit(int[] stockPrices) {
+
+        int currentStock = stockPrices[0];
+        int totalProfit = 0;
+        for (int i = 1; i < stockPrices.length; i++) {
+            if (stockPrices[i] < currentStock) {
+                currentStock = stockPrices[i];
+            }
+            int diff = stockPrices[i] - currentStock;
+            if (diff > totalProfit) {
+                totalProfit = diff;
+            }
+        }
+        return totalProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] stockPrices = {7, 1, 5, 3, 6, 4};
+        System.out.println(findMaxProfit(stockPrices));
+    }
+}
